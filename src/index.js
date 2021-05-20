02620662e67a844db75241b6f8e814beddf1952e
ohtAIrobo09tgc <67031867+ohtAIrobo09tgc@users.noreply.github.com>
2021-05-20 16:15:52 +0900

    電卓機能本格実装

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener('keydown', (event) => {
-    console.log(event.key);
-  }); 
+document.addEventListener('DOMContentLoaded', () => {
+  const $display = document.getElementById('display');
+  
+  for (const $e of document.getElementsByClassName('number-button')) {
+    $e.addEventListener('click', () => {
+      console.log('Clicked:', $e.textContent);
+      
+      if ($display.textContent === '0' && $e.textContent === '0') {
+
+      }
+      
+      else if ($display.textContent === '0') {
+        $display.textContent = $e.textContent;
+      }
+
+      else {
+        $display.textContent = $display.textContent + $e.textContent;
+      }
+    });
+  }
+
+  for (const $e of document.getElementsByClassName('operator')) {
+    $e.addEventListener('click', () => {
+      console.log('Clicked:', $e);
+    });
+  }
+
+  document.getElementById('equal')
+    .addEventListener('click', () => {
+      console.log('Clicked: =');
+    });
+});    
