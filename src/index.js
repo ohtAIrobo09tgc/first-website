c626557603885dd84e4abbb3ab69a62f5f9a6479
ohtAIrobo09tgc <67031867+ohtAIrobo09tgc@users.noreply.github.com>
2021-06-03 15:56:35 +0900

    java編集

--- src/index.js
+++ src/index.js
@@ -1,32 +1,70 @@
+let operatorPressed = false;
+let inputs = [];
+
 document.addEventListener('DOMContentLoaded', () => {
   const $display = document.getElementById('display');
-  
+
   for (const $e of document.getElementsByClassName('number-button')) {
     $e.addEventListener('click', () => {
-      console.log('Clicked:', $e.textContent);
-      
-      if ($display.textContent === '0' && $e.textContent === '0') {
-
-      }
-      
-      else if ($display.textContent === '0') {
+      if (operatorPressed) {
         $display.textContent = $e.textContent;
+        operatorPressed = false;
       }
-
       else {
-        $display.textContent = $display.textContent + $e.textContent;
+        if ($display.textContent === '0' && $e.textContent === '0') {
+          // 何もしない
+        }
+        
+        else if ($display.textContent === '0') {
+          $display.textContent = $e.textContent;
+        }
+        
+        else {
+          $display.textContent = $display.textContent + $e.textContent;
+        }
       }
     });
   }
 
   for (const $e of document.getElementsByClassName('operator')) {
     $e.addEventListener('click', () => {
-      console.log('Clicked:', $e);
+      if(!operatorPressed) {
+        inputs.push($display.textContent);
+        operatorPressed = true;
+        inputs.push($e.textContent);
+      }
+      else{
+        inputs[inprts.length - 1] = $e.textContent;
+      }
     });
   }
 
   document.getElementById('equal')
     .addEventListener('click', () => {
-      console.log('Clicked: =');
+      inputs.push($display.testContent);
+      
+      operand = '';
+      before = Number(inpurts[0]);
+
+      for (const e of inputs.slice(1)) {
+        if (e === '+' || e === '-' || e === '×' || e === '÷'){
+          operand = e;
+        }
+        else{
+          if(operand === '+') {
+            before = before + Number(e);
+          }
+          else if (operand === '-') {
+            before = before + Number(e);
+          }
+          else if (operand === '×') {
+            before = before + Number(e);
+          }
+          else if (operand === '÷') {
+            before = before + Number(e);
+          }
+        }
+      }
+      console.log(before);
     });
-});    
+});
