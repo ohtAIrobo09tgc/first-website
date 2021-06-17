55eabcafe62dfc46d3489e351cd1bf428df13309
ohtAIrobo09tgc <67031867+ohtAIrobo09tgc@users.noreply.github.com>
2021-06-17 15:22:33 +0900

    電卓機能の正常運用化・改良開始

--- src/index.js
+++ src/index.js
@@ -7,24 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
   for (const $e of document.getElementsByClassName('number-button')) {
     $e.addEventListener('click', () => {
-      
+      // 直前に '＝' が押されている場合の処理
       if (equalPressed) {
         $display.textContent = $e.textContent;
         equalPressed = false;
       }
-      
+      // 直前に '＋', '－', '×', '÷' が押されている場合の処理
       else if (operatorPressed) {
         $display.textContent = $e.textContent;
         operatorPressed = false;
       }
-      
+      // 直前に数字が押されている場合の処理
       else {
-        if ($display.textContent === '0' && $e.textContent === '0') {
-          
-        }
-        else if ($display.textContent === '0') {
+        // => 0 の場合に、その数字に入れ替わるようにしたい
+        if ($display.textContent === '0') {
           $display.textContent = $e.textContent;
         }
+        // => 通常の場合 (ケタが増えていく)
         else {
           $display.textContent = $display.textContent + $e.textContent;
         }
@@ -34,13 +33,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   for (const $e of document.getElementsByClassName('operator')) {
     $e.addEventListener('click', () => {
+      if($e.textContent==='.'){
+        console.log('.入力')
+      }
       equalPressed = false;
       
+      // 直前に数字が押されている場合の処理
       if (!operatorPressed) {
         inputs.push($display.textContent);
         operatorPressed = true;
         inputs.push($e.textContent);
       }
+      // 直前に '＋', '－', '×', '÷' が押されている場合の処理
       else {
         inputs[inputs.length - 1] = $e.textContent;
       }
@@ -54,11 +58,12 @@ document.addEventListener('DOMContentLoaded', () => {
       equalPressed = true;
     
       inputs.push($display.textContent);
-              
-      operand = '';
-      before = Number(inputs[0]);
+      console.log(inputs)        
+      let operand = '';
+      let before = Number(inputs[0]);
       
       for (const e of inputs.slice(1)) {
+        console.log(e)
         if (e === '＋' || e === '－' || e === '×' || e === '÷') {
           operand = e;
         }
@@ -77,6 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         }
       }
+      console.log(before)
       
       inputs = [];
       $display.textContent = before;
